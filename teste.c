c44cd8174c10f71a720f57e3dc3056e5e1eaccbb
Bernardo <bernardot@ua.pt>
2023-03-07 11:02:15 +0000

    teste.c improved skeleton

--- teste.c
+++ teste.c
@@ -1,27 +1,30 @@
-void main(void) {
+#include "FIFO.h"
 
-    
-    int option;
+void main(void) {
+    int option,size,data,free;
     while(1) {
         printf("Choose an option: \n1:MyFIFOInit()\n2:MyFIFOInsert()\n3:MyFIFORemove()\n4:MyFIFOPeep\n5:MyFIFOSize\n");
         option = readInt();
         switch(option)  {
             case(1):
                 printf("FIFO size:");
-                int size = readInt();
-                FIFOInit(size);
+                size = readInt();
+                MyFIFOInit(size);
                 break;
             case(2):
-
+                printf("Insert data");
+                data = readInt();
+                MyFIFOInsert(data);
                 break;
             case(3):
-
+                data = MyFIFORemove();
+                printInt(data);
                 break;
             case(4):
-
+                MyFIFOPeep();
                 break;
             case(5):
-
+                MyFIFOSize();
                 break;
         }
     }
