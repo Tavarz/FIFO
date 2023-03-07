f467de35981c7294709521b9cd4604ee95f9b603
JPCR16 <jpcr@ua.pt>
2023-03-07 10:35:44 +0000

    Fifo header

---
+++ FIFO.h
@@ -0,0 +1,14 @@
+#ifndef __FIFO__H_
+#define __FIFO__H_
+
+#include <stdio.h>
+#include <stdint.h>
+#include <math.h>
+
+void MyFIFOInit(int size);
+int MyFIFOInsert(int data);
+int MyFIFORemove();
+int MyFIFOPeep();
+int MyFIFOSize();
+
+#endif
