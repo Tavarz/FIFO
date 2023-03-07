ccf322750833f48917fa05dbab39dc0c0b4ba6d2
Bernardo <bernardot@ua.pt>
2023-03-07 10:27:55 +0000

    teste.c added

--- FIFO.c
+++ FIFO.c
@@ -1,34 +1,6 @@
 
 
-void main(void) {
 
-    
-    int option;
-    while(1) {
-        printf("Choose an option: \n1:MyFIFOInit()\n2:MyFIFOInsert()\n3:MyFIFORemove()\n4:MyFIFOPeep\n5:MyFIFOSize\n");
-        option = readInt();
-        switch(option)  {
-            case(1):
-                printf("FIFO size:");
-                int size = readInt();
-                FIFOInit(size);
-                break;
-            case(2):
-
-                break;
-            case(3):
-
-                break;
-            case(4):
-
-                break;
-            case(5):
-
-                break;
-        }
-    }
-    
-}
 
 void MyFIFOInit(int size) {
 
