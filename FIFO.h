7bc3905f3b4959360cf036e3e0cbcbf2126e56a8
JPCR16 <jpcr@ua.pt>
2023-03-11 12:14:20 +0000

    Header file documented

--- FIFO.h
+++ FIFO.h
@@ -1,3 +1,13 @@
+/**
+ * \file:   FIFO.h
+ * \author: Bernardo Tavares <bernardot@ua.pt> and João Rodrigues <jpcr@ua.pt>
+ *
+ * \date 11 March 2023
+ * 
+ * \brief Contains functions to initialize a FIFO and insert/remove elements from it
+ *        Also has functions to return the oldest element on the fifo and return its size
+ */
+
 #ifndef __FIFO__H_
 #define __FIFO__H_
 
@@ -5,11 +15,84 @@
 #include <stdint.h>
 #include <math.h>
 
+/**
+ * \brief Creates an array "on" of flags to check if a FIFO has been already initialized
+ *        Also creates 2 arrays to save each FIFO's size and tail
+ * 
+ * \return       Doesn't return anything
+ */
+
 void MyFIFOStart(void);
+
+/**
+ * \brief Initializes a FIFO with size "size"
+ * 
+ * Initially checks if the chosen FIFO has already been initialized. If it has
+ * prints "Array already initialized". 
+ * If not, initializes the chosen FIFO with input argument "size".
+ * Also saves the size of the FIFO in the array that saves all FIFO's sizes.
+ * 
+ * \param FIFO   FIFO array number in the array of FIFOs
+ * \param size   Size of FIFO
+ * \return       Doesn't return anything
+ */
+
 void MyFIFOInit(int FIFO, int size); 
+
+/**
+ * \brief Adds an elemnt to the FIFO
+ * 
+ * Initially checks if the chosen FIFO is full with the help of the array's
+ * that save all FIFO's sizes and tails. If it's full prints a message saying
+ * it's full.
+ * If it's not full adds the new element to the last position in the array and
+ * increments the tail of this FIFO.
+ * 
+ * \param FIFO   FIFO array number in the array of FIFOs
+ * \param data   User input data
+ * \return       Doesn't return anything
+ */
+
 void MyFIFOInsert(int FIFO,int data); 
+
+/**
+ * \brief Removes an element from the FIFO
+ * 
+ * Saves the FIFO's first value in a variable "data", sets the first value to 0 and 
+ * then shifts all values to the left (1 position). 
+ * After it updates FIFO's tail value by decrementing the tail value by 1 and 
+ * sets the last value of the FIFO to 0.
+ * 
+ * \param FIFO   FIFO array number in the array of FIFOs
+ * \return       Returns the element being removed
+ */
+
 int MyFIFORemove(int FIFO); 
+
+/**
+ * \brief Returns the oldest element on the FIFO without removing it
+ * 
+ * Checks if the FIFO has been initialized before:
+ * If it hasn't prints "FIFO not initialized"
+ * If it has returns the FIFO's first element
+ * 
+ * \param FIFO   FIFO array number in the array of FIFOs
+ * \return       Returns the FIFO's first element 
+ */
+
 int MyFIFOPeep(int FIFO);
+
+/**
+ * \brief Returns the number of elements on the FIFO
+ * 
+* Checks if the FIFO has been initialized before:
+ * If it hasn't prints "FIFO not initialized"
+ * If it has returns the number of elements on the FIFO
+ * 
+ * \param FIFO   FIFO array number in the array of FIFOs
+ * \return       Returns the number of elements on the FIFO
+ */
+
 int MyFIFOSize(int FIFO);
 
 #endif
