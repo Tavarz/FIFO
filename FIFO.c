2ad45ee149f29e784ee583c16a91ed59e82885e0
Bernardo <bernardot@ua.pt>
2023-03-11 09:46:12 +0000

    Start Init Insert Remove Peep

--- FIFO.c
+++ FIFO.c
@@ -3,42 +3,81 @@
 #include <stdint.h>
 #include <math.h>
 
-int fifo[0];
-int p=0;
+#ifndef MAX_FIFO
+#define MAX_FIFO 10
 
-void MyFIFOInit(int size) {
+static int start=0;
+static int MyFIFO[MAX_FIFO][0];
+static int on[MAX_FIFO];
+static int FIFOSize[MAX_FIFO];
+static int tail[MAX_FIFO];
 
-    fifo[size];
-    int i;
-    for(i = 0; i < size; i++){
-        
-        fifo[i] = 0;
+void MyFIFOStart(void) {
+    start = 1;
+    for(int i = 0; i < MAX_FIFO; i++) {
+        on[i] = 0;
+        FIFOSize[i] = 0;
+        tail[i] = 0;
+    }
+}
 
+void MyFIFOInit(int FIFO, int size) {
+    if(start == 0) {
+        MyFIFOStart();
     }
 
-}
-
-void MyFIFOInsert(int data) {
-    if(p > sizeoffifo)){
-        printf("FIFO full!");
-        exit();
+    if(on[FIFO] == 1){
+        printf("Array already Initialized.\n");
     }
-    fifo[p] = data;
-    p++;
-}
-
-int MyFIFORemove() {
-    int data = fifo[0];
-    for(int k = 1; k < sizeoffifo ; k++){
-        fifo[k-1] = fifo[k];
+    else
+    {
+        on[FIFO] = 1;
+        FIFOSize[FIFO] = size;
+        for(int i = 0; i < size; i++){
+            MyFIFO[FIFO][i] = 0;
+        }
     }
-    return data;
 }
 
-int MyFIFOPeep() {
-    return fifo[0];
+void MyFIFOInsert(int FIFO,int data) {
+    if(on[FIFO] == 1) {
+        if(tail[FIFO] == FIFOSize[FIFO]){
+            printf("FIFO %f is full.\n",FIFO);
+        }
+        else{
+            MyFIFO[FIFO][tail[FIFO]] = data;
+            tail[FIFO] = tail[FIFO] + 1;
+        }
+    }
+    else
+    {
+        printf("FIFO %f not initialized.\n",FIFO);
+    }
 }
 
-int MyFIFOSize() {
+int MyFIFORemove(int FIFO) {
+    if(on[FIFO] == 1) {
+        if(tail[FIFO] != 0) {
+            int data = MyFIFO[FIFO][0];
+
+            for(int i = 1; i < tail[FIFO]; i++) {
+                MyFIFO[FIFO][i-1] = MyFIFO[FIFO][i];
+            }
+            tail[FIFO] = tail[FIFO] - 1;
+        }
+    }
+    else
+    {
+        printf("FIFO %f not initialized.\n",FIFO);
+    }
+}
+
+int MyFIFOPeep(int FIFO) {
+    return MyFIFO[FIFO][tail[FIFO]];
+}
+
+int MyFIFOSize(int FIFO) {
     return p-1;
 }
+
+#endif
